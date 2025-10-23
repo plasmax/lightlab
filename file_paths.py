1fb2959f3e616298a24cbd7958e5da041ba2218b
Max Last <tsalxam@gmail.com>
2025-10-23 12:31:39 +0100

    Create file_paths.py

---
+++ file_paths.py
@@ -0,0 +1,8 @@
+# example expected patterns - these need to be modified
+patterns = {
+    "normal": "/path/to/ll_normal.%04d.jpeg",
+    "depth":  "/path/to/ll_depth.%04d.jpeg",
+    "only":   "/path/to/ll_light_only.%04d.jpeg",
+    "off":    "/path/to/ll_light_off.%04d.jpeg",
+    "mask":   "/path/to/ll_light_mask.%04d.jpeg",
+}
